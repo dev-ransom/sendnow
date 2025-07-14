a1df6525c57bd7bb162ff245c4e1eed24dba4b53
Big Ransom <sunday@youngmillionvoices.org>
2025-07-14 12:25:38 +0100

    added responsiveness

--- app/(pages)/auth/verify-email/page.tsx
+++ app/(pages)/auth/verify-email/page.tsx
@@ -3,13 +3,12 @@ import VerifyCode from "@/app/components/verify-email";
 const page = () => {
   return (
     <div className="flex flex-col justify-center items-center h-full px-[5%]">
-      <div className="w-full flex justify-center items-center">
-        <div className="flex-1 h-[600px]">
-          <img src="/images/verifycode.png" alt="" />
+      <div className="w-full flex justify-center items-center flex-col">
+        <div className="h-[500px] lg:w-[600px] w-full flex-1">
+          <img src="/images/verifycode.png" alt="" className="w-full h-full" />
         </div>
         <div className="flex-1">
-
-        <VerifyCode />
+          <VerifyCode />
         </div>
       </div>
     </div>
