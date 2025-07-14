49a1d77b93cf91482f2918457e67351c6fecc1f5
Big Ransom <sunday@youngmillionvoices.org>
2025-07-14 12:45:22 +0100

    added code for desktop view

--- app/(pages)/auth/verify-email/page.tsx
+++ app/(pages)/auth/verify-email/page.tsx
@@ -3,7 +3,7 @@ import VerifyCode from "@/app/components/verify-email";
 const page = () => {
   return (
     <div className="flex flex-col justify-center items-center h-full px-[5%]">
-      <div className="w-full flex justify-center items-center flex-col">
+      <div className="w-full flex justify-center items-center lg:flex-row flex-col">
         <div className="h-[500px] lg:w-[600px] w-full flex-1">
           <img src="/images/verifycode.png" alt="" className="w-full h-full" />
         </div>
